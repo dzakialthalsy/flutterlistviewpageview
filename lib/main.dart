b3460a2bd27e4bfb5e42591d5de0892b0077d7a6
Dzaki Althalsyah <dz4ic7@gmail.com>
2026-04-01 14:06:13 +0700

    Changes on title

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Data Mahasiswa',
+      title: 'Sistem Informasi Akademik Mahasiswa',
       theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
       home: const HalamanDaftarMahasiswa(),
     );
